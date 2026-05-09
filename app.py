f35a60962f6803eeb2d78c335ed375b7ec5416b6
Anushka Shakya <chiyaa2005@gmail.com>
2026-05-09 21:07:13 +0530

    render deployment setup

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def home():
     )
 
 
-if __name__ == "__main__":
+import os
 
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
